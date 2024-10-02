05cd9fb7c23de3ca86930f632b1d19d91af4c78e
smirta-pathak <smirta.pathak12056@gmail.com>
2024-10-02 22:54:05 +0530

    Add post http method

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express');          
+const bodyParser = require("body-parser");
 
-let tasks=[];
+const app = express();
+app.use(bodyParser.json());    // using bodyParser to parse POST json data
+
+let taskArray=[];
 let count=0;
 
 app.get('/tasks',(req,res)=>{
-    res.status(200).json(tasks);
+    res.status(200).json(taskArray);
+});
+
+app.post("/tasks", (req, res) => {
+    const task = req.body;
+    let newTask = { ...task, id: ++count };
+    taskArray.push(newTask);
+    res.status(201).json(newTask);
 });
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
+
+
 
